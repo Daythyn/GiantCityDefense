5da400c810a321025d67ef85f3fbba11bb2e35b4
Daythyn <dprice10@stu.jsu.edu>
2024-12-08 21:39:37 -0500

    Resources Yaaay

--- Assets/Scripts/ResourceSpawn.cs
+++ Assets/Scripts/ResourceSpawn.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class ResourceSpawn : MonoBehaviour
 {
@@ -9,26 +10,37 @@ public class ResourceSpawn : MonoBehaviour
 
     public GameObject resource;
     public Vector3 attachPos;
-    Boolean hasResource;
+    public Boolean hasResource;
 
     public float cooldown;
     public float cooldownCurrent;
+
+    XRSocketInteractor socket;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasResource = false;
+        socket = this.GetComponent<XRSocketInteractor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(cooldown > 0){
-            reloadCurrent -= Time.deltaTime;
-        } else if (canShoot && targetCurrent != null){
-            canShoot = false;
-            shoot();
+
+        if(socket.hasSelection){
+            hasResource = true;
+        } else{
+            hasResource = false;
+        }
+
+        if(cooldownCurrent > 0 && hasResource == false){
+            cooldownCurrent -= Time.deltaTime;
+        } else if (!hasResource){
+            GameObject proj = Instantiate(resource, transform.position, transform.rotation);
+            cooldownCurrent = cooldown;
         }
     }
+
 }
